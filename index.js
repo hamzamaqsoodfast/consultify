09fafbf258a1e53b7469c96750874e21b11403b3
abdullahawais26 <130307989+abdullahawais26@users.noreply.github.com>
2024-05-07 23:52:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -457,3 +457,46 @@ app.get('/sendclientlogin',async (req,res)=>
             res.status(500).json({ message: 'Failed to update availability' });
         }
     });
+app1.get('/checkprofile', async (req, res) => {
+
+    try {
+        const username = req.query.username; // Access the text1 query parameter sent from the client
+console.log(username);
+  
+        // Compare the hashed password in the database
+        const [rows] = await pool.execute(
+            'SELECT * FROM Doctors WHERE username = ?',
+            [username] // Directly use the user-provided password
+        );
+
+        if (rows.length > 0) {
+          
+            const response = {
+               dataprofile:rows
+            };
+            console.log(response);
+
+            wss.clients.forEach((client) => {
+                client.send(JSON.stringify(response));
+            });
+
+
+        }
+        else {
+            let loginstatus = "Incorrect";
+            const response = {
+                loginstatus: loginstatus,
+            };
+            wss.clients.forEach((client) => {
+                client.send(JSON.stringify(response));
+            });
+            //   console.log(response);
+
+
+        }
+
+
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+});
